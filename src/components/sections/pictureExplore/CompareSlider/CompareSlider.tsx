3f5f9f310b5ea3d09f51dd828ad417f9efbb5e64
John Smoliak <poznavauka@mail.ru>
2022-07-18 16:08:43 +0300

    refactor: change components structure

---
+++ src/components/sections/pictureExplore/CompareSlider/CompareSlider.tsx
@@ -0,0 +1,44 @@
+import { FC, ReactElement } from 'react';
+import {
+  ReactCompareSlider,
+  ReactCompareSliderImage,
+  styleFitContainer
+} from 'react-compare-slider';
+
+import before from '../img/before.jpg';
+import after from '../img/after.jpg';
+
+import './CompareSlider.scss';
+
+type CompareSliderPropsTypes = {
+  imageTitle: string;
+};
+
+export const CompareSlider: FC<CompareSliderPropsTypes> = ({
+  imageTitle
+}): ReactElement => (
+  <div className='picture-explore__compare-slider compare-slider'>
+    <ReactCompareSlider
+      style={{
+        cursor: 'grab',
+        ...styleFitContainer({
+          cursor: 'grab'
+        })
+      }}
+      itemOne={
+        <ReactCompareSliderImage
+          src={before}
+          title={imageTitle}
+          alt={imageTitle}
+        />
+      }
+      itemTwo={
+        <ReactCompareSliderImage
+          src={after}
+          title={imageTitle}
+          alt={imageTitle}
+        />
+      }
+    />
+  </div>
+);
---
+++ src/components/sections/pictureExplore/DescriptionBlock/DescriptionBlock.tsx
@@ -0,0 +1,34 @@
+import { FC, ReactElement } from 'react';
+
+import './DescriptionBlock.scss';
+
+type DescriptionBlockPropsTypes = {
+  heading: string;
+  firstRow: string;
+  secondRowFirstHalf: string;
+  highlightedText: string;
+  secondRowSecondHalf: string;
+  thirdRow: string;
+};
+
+export const DescriptionBlock: FC<DescriptionBlockPropsTypes> = ({
+  heading,
+  firstRow,
+  secondRowFirstHalf,
+  highlightedText,
+  secondRowSecondHalf,
+  thirdRow
+}): ReactElement => (
+  <div className='picture-explore__picture-content picture-content'>
+    <h2 className='picture-content__heading'>{heading}</h2>
+    <div className='picture-content__description picture-description '>
+      <p className='picture-description__text'>{firstRow}</p>
+      <p className='picture-description__text'>
+        {secondRowFirstHalf}&nbsp;
+        <span className='highlighted-text'>{highlightedText}</span>&nbsp;
+        {secondRowSecondHalf}
+      </p>
+      <p className='picture-description__text'>{thirdRow}</p>
+    </div>
+  </div>
+);

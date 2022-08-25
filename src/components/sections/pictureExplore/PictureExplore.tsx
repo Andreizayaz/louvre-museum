ee1088ccf5c2458e2143a2f2c5fd52a50ca187e8
John Smoliak <poznavauka@mail.ru>
2022-08-25 18:35:25 +0300

    feat: create picture explore section adaptive

--- src/components/sections/pictureExplore/PictureExplore.tsx
+++ src/components/sections/pictureExplore/PictureExplore.tsx
@@ -34,7 +34,6 @@ export const PictureExplore: FC<PictureExplorePropsTypes> = ({
       ref={pictureExploreRef}
       className='picture-explore'
       id='picture-explore'
-      style={{ display: 'none' }}
     >
       <div
         className={`picture-explore__container container ${
